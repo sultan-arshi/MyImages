9ede30f415225ae5acf028d59298e65bfb7868b9
Rashdan Natiq <rashdan@natiq.se>
2017-12-01 17:47:34 +0100

    Initial Commit

---
+++ MyImages/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyImages
+//
+//  Created by Rashdan Natiq on 2017-12-01.
+//  Copyright © 2017 Sultan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
